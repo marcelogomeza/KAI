ef07ca67e786946c2d11b2e7edab930ddd2a1654
Marcelo <marcelogomeza@gmail.com>
2026-02-20 16:34:42 -0500

    push

---
+++ server/dist/src/plugins/S3Plugin.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.S3Plugin = void 0;
+class S3Plugin {
+    name = 'AWS S3';
+    async listFiles() {
+        return ['backup-2026-01-01.zip', 'user-upload-72.png'];
+    }
+    async uploadString(filename, content) {
+        console.log(`[AWS S3] Uploaded ${filename} to bucket`);
+        return true;
+    }
+    async downloadString(filename) {
+        return `Simulated content from S3 object: ${filename}`;
+    }
+}
+exports.S3Plugin = S3Plugin;
